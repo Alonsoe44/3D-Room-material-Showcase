36770a4fa9f705379a278607979ddca3439f622b
Emiliano Polanco Baquerizo <emilianopolanco5@gmail.com>
2022-12-10 14:08:23 +0100

    Adding jest to the project and testing a button

---
+++ src/Button.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Button from './button'
+import '@testing-library/jest-dom'
+
+describe('Given a button component', () => {
+  describe("When it's rendered", () => {
+    test('Then it should display some text', () => {
+      render(<Button />)
+
+      const foundText = screen.getByRole('button', { name: /magic/i })
+
+      expect(foundText).toBeInTheDocument()
+    })
+  })
+})
